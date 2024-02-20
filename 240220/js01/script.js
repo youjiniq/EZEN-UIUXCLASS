d3254f459a14d6378b80e2d78b901e2d50594dc7
youjiniq <9991126@naver.com>
2024-02-20 17:41:47 +0900

    240220

---
+++ 240220/js01/script.js
@@ -0,0 +1,15 @@
+// const testNum = Number.MAX_SAFE_INTEGER;
+// const testNum01 = Number.MIN_SAFE_INTEGER;
+// console.log(testNum);
+// console.log(testNum01);
+
+// const testNum02 = BigInt(Number.MAX_SAFE_INTEGER) + 1n;
+// const testNum03 = BigInt(Number.MIN_SAFE_INTEGER);
+
+// console.log(testNum02);
+// console.log(testNum03);
+
+// DOM = 시스템
+// Document Object Model 문서 객체 모델 시스템
+// Document = html 문서 
+
