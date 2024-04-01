500f297bd53ec2aca947b395f48bf5ea50008088
youjiniq <9991126@naver.com>
2024-04-01 18:35:16 +0900

    240401

---
+++ 240329/js01/script.js
@@ -0,0 +1,51 @@
+const staggerWrap = document.querySelector("ul");
+const fragment = document.createDocumentFragment();
+
+const grid = [9, 5];
+const [col, row] = grid;
+const allElem = row * col;
+
+for (let i = 0; i < allElem; i++) {
+  const li = document.createElement("li");
+  fragment.appendChild(li);
+}
+
+staggerWrap.appendChild(fragment);
+
+// anime({
+//   targets: "ul li",
+//   easing: "linear",
+//   duration: 1000,
+//   scale: anime.stagger([0.5, 1], {
+//     grid: [9, 3],
+//     from: "center",
+//     axis: "z",
+//   }),
+// });
+
+const tl = anime.timeline({
+  targets: "ul li",
+  delay: anime.stagger(200, { grid: [9, 5], from: "center", axis: "z" }),
+  loop: true,
+  direction: "alternate",
+});
+tl.add({
+  scale: [
+    { value: 0.1, easing: "easeOutSine", duration: 500 },
+    { value: 1, easing: "easeOutQuad", duration: 1200 },
+  ],
+})
+.add({
+  translateX: anime.stagger(10, {
+    grid: [9, 5], from: "center", axis: "x",
+  }),
+  translateY: anime.stagger(10, {
+    grid: [9, 5], from: "center", axis: "y",
+  }),
+  rotate: anime.stagger([0, 90], {grid: [9, 5], from: "center",axis: "x" }),
+});
+
+
+// anime.js : 보편 / 무난 / 쉬움
+// gsap.js : 중급 / 기능 다양
+// three.js : 2D + 3D 그래픽 / 고급 / 기능 다양 => 4D
---
+++ 240401/react01/src/components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div><h1>Footer</h1></div>;
+};
+
+export default Footer;
