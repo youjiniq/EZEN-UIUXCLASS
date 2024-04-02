7f4ec94fb769b73386b35949abfebb936d35b5dc
youjiniq <9991126@naver.com>
2024-04-02 18:02:48 +0900

    240402

---
+++ 240402/02react/src/App.js
@@ -0,0 +1,68 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  // const [name, setName] = useState("");
+  // const onChangeName = (e) => {
+  //   setName(e.target.value);
+  // };
+  // const [gender, setGender] = useState("");
+  // const onChangeGender = (e) => {
+  //   setGender(e.target.value);
+  // };
+  // const [birth, setBirth] = useState("");
+  // const onChangeBirth = (e) => {
+  //   setBirth(e.target.value);
+  // };
+  // const [bio, setBio] = useState("");
+  // const onChangeBio = (e) => {
+  //   setBio(e.target.value);
+  // };
+
+  const [state, setState] = useState({
+    name: "",
+    gender: "",
+    birth: "",
+    bio: "",
+  });
+
+  const handleOnChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <input
+          name="name"
+          value={state.name}
+          placeholder="이름"
+          onChange={handleOnChange}
+        />
+      </div>
+      <div>
+        <select name="gender" value={state.gender} onChange={handleOnChange}>
+          <option key={""}></option>
+          <option key={"여성"}>여성</option>
+          <option key={"남성"}>남성</option>
+        </select>
+      </div>
+      <div>
+        <input
+          name="birth"
+          value={state.birth}
+          type="date"
+          onChange={handleOnChange}
+        />
+      </div>
+      <div>
+        <textarea name="bio" value={state.bio} onChange={handleOnChange} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
